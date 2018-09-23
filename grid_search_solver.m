5de5000678a998caee4402e13e4a778115f14a60
niranjir <niranjir@andrew.cmu.edu>
2018-09-23 13:45:52 -0400

    modified for range only from subset of beacons

--- grid_search_solver.m
+++ grid_search_solver.m
@@ -1,14 +1,20 @@
-function [EstPos, MinErr] = grid_search_solver(BeaconPos, Range, GridRes, BoundingBox)
+function [EstPos, MinErr] = grid_search_solver(BeaconPos, Data_In, GridRes, BoundingBox)
 % This function performs grid seach
 % Input to solver:
 %   BeaconPos   : Nbx3 matrix where Nb is number of beacons
-%   Range       : Nbx1 matrix
+%   Data_In     : Nrx2 matrix where Nr is the number of beacons in range
+%                   First column is index of beacons in range. Index is
+%                   from 1 to Nb
+%                   Second column is measured range
 %   GridRes     : 1x1 matrix
 %   BoundingBox : 2x3 matrix with [Xmin Ymin Zmin; Xmax Ymax Zmax]
 % Output from solver:
 %   EstPos      : 1x3 matrix with estimated position
 %   Residue     : 1x1 matrix with residue error - this is an approx indication of
 %                   confidence of measurement
+
+BeacInd       = Data_In(:,1);
+Range         = Data_In(:,2);
 
 
 % Generate all the points in the grid
@@ -19,7 +25,7 @@ function [EstPos, MinErr] = grid_search_solver(BeaconPos, Range, GridRes, Boundi
 All_xyz = [AllPts_x(:) AllPts_y(:) AllPts_z(:)];
 
 % Compute distance to beacons for all points
-TrueDist = pdist2(All_xyz,BeaconPos);
+TrueDist = pdist2(All_xyz,BeaconPos(BeacInd,:));
 
 % Compute error between true and meaured distance
 Err_TrueEstDist = sum(abs(bsxfun(@minus, TrueDist,Range')),2);
--- main.m
+++ main.m
@@ -37,33 +37,47 @@ RxPos = [Xmin + (Xmax-Xmin)*rand...
 % Define standard deviation in range noise
 range_std = 0.3;
 
+% Select a subset of beacons
+BeacInd = unique(randi([1 Nb],5,1)); % Selecting at most 5 beacons
+BeacInRange = BeaconPos(BeacInd,:)
+
 % Generate true range from all beacons
-Range = pdist2(RxPos,BeaconPos)';
-MeasuredRange = Range + range_std*randn(Nb,1);
+Range = pdist2(RxPos,BeacInRange)';
+MeasuredRange = Range + range_std*randn(numel(BeacInd),1);
 
 % Resolution of search space
 GridRes = 0.05; 
 
 % ------------- Call Solver ------------------------------------------
 % Input to solver:
-%   BeaconPos   : Nbx3 matrix where Nb is number of beacons
-%   MeasuredRange : Nbx1 matrix
+%   BeaconPos   : Nbx3 matrix where Nb is number of beacons. 3D position of
+%                   all beacons
+%   BeacInd     : Nrx1 matrix where Nr is number of beacons in range. This
+%                   is the index of all beacons in range, index ranging from 1 to Nb
+%   MeasuredRange : Nrx1 matrix  where Nr is number of beacons in range.
+%                   This is the measured range from the beacons with index
+%                   BeacInd
 %   GridRes     : 1x1 matrix
 %   BoundingBox : 2x3 matrix with [Xmin Ymin Zmin; Xmax Ymax Zmax]
 % Output from solver:
 %   EstPos      : 1x3 matrix with estimated position
 %   Residue     : 1x1 matrix with residue error - this is an approx indication of
 %                   confidence of measurement
-[EstPos, Residue] = grid_search_solver(BeaconPos, MeasuredRange, GridRes, BoundingBox);
+[EstPos, Residue] = grid_search_solver(BeaconPos, [BeacInd MeasuredRange], GridRes, BoundingBox);
 RxPos
 EstPos
 Residue
 % -------------- Plot results ---------------------------------------
-figure; scatter3(BeaconPos(:,1),BeaconPos(:,2),BeaconPos(:,3),100,'filled','MarkerFaceColor','b','MarkerEdgeColor','b');
-axis equal; hold on;
+figure; hold on;
+scatter3(BeaconPos(:,1),BeaconPos(:,2),BeaconPos(:,3),100,'filled','MarkerFaceColor',[0.6 0.6 0.6],'MarkerEdgeColor',[0.6 0.6 0.6]);
+scatter3(BeaconPos(BeacInd,1),BeaconPos(BeacInd,2),BeaconPos(BeacInd,3),100,'filled','MarkerFaceColor','b','MarkerEdgeColor','b');
+axis equal; grid on; 
+xlabel('x'); ylabel('y'); zlabel('z');
 scatter3(RxPos(1),RxPos(2),RxPos(3),'g');
 scatter3(EstPos(1), EstPos(2), EstPos(3),'r');
-legend({'Beacons';'True Pos';'Est Pos'});
+legend({'Beacons not in range';'Beacons in range';'True Pos';'Est Pos'});
+set(gca,'fontsize',14);
+title('3D plot of true and estimated locations');
 
 
 
